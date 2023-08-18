fea3003d9ddff7da1552b737ef7b5f39369e0051
MahmoudAliEid <alim46972@gmail.com>
2023-08-18 19:45:52 +0300

    resolve some errors

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
-import "@styles/globals.css";
+import "@/styles/globals.css";
 
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
+import Nav from "@/components/Nav";
+import Provider from "@/components/Provider";
 
 export const metadata = {
   title: "Promptron",
